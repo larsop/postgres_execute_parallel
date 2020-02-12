e5ff8b03fd92a08581f5daf906ad568b164c5dfc
Lars Aksel Opsahl <lop@skogoglandskap.no>
2020-02-12 13:29:34 +0100

    Test update

--- src/test/sql/regress/execute_parallel.sql
+++ src/test/sql/regress/execute_parallel.sql
@@ -6,8 +6,8 @@ select 2, execute_parallel('{"select aaaapg_sleep(10)","select pg_sleep(1)","sel
 select 3, execute_parallel('{"create table test(c1 int)"}'::text[],3);
 select 4, execute_parallel('{"insert  into testfeil) values (2)","insert  into test(c1) values (1)","insert  into test(c1) values (1)","insert  into test(c1) values (1)","insert  into test(c1) values (1)","insert  into test(c1) values (1)","insert  into test(c1) values (1)"}'::text[],3);
 select 5, count(*) from test where c1=1;
-select 5, count(*) from test where c1=2;
-select 6, execute_parallel('{"insert  into test(c1) values (1)","insert  into test(c1) values (1)","insert  into test(c1) values (1)","insert  into test(c1) values (1)","insert  into test(c1) values (1)","insert  into test(c1) values (1)"}'::text[],10);
-select 7, count(*) from test where c1=1;
-select 8, count(*) from test where c1=2;
+select 6, count(*) from test where c1=2;
+select 7, execute_parallel('{"insert  into test(c1) values (1)","insert  into test(c1) values (1)","insert  into test(c1) values (1)","insert  into test(c1) values (1)","insert  into test(c1) values (1)","insert  into test(c1) values (1)"}'::text[],10);
+select 8, count(*) from test where c1=1;
+select 9, count(*) from test where c1=2;
 drop table test;
